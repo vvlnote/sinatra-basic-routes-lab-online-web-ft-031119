425cb63fa2476986e2aaf191397b6e0050cd90da
Joy Lin <vvlnote@hotmail.com>
2019-04-27 22:06:19 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do
+    "Hello, World!"
+  end
+  
+  get '/name' do
+    "My name is Joy"
+  end
+  
+  get '/hometown' do
+    "My hometown is Taipei"
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is Hello"
+  end
 end
